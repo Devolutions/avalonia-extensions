01752dd0cb5249fae586b01bf6a94f5664dbbdcf
apman <amalchowperryman@devolutions.net>
2025-08-19 17:37:53 -0400

    [DevExpress] Change TabControl VerticalContentAlignment
    plus a bunch of changes in the demos to better illustrate/test alignment issues

--- samples/SampleApp/DemoPages/ListBoxDemo.axaml.cs
+++ samples/SampleApp/DemoPages/ListBoxDemo.axaml.cs
@@ -12,7 +12,10 @@ public partial class ListBoxDemo : UserControl
         this.animals1.ItemsSource = this.animals2.ItemsSource = new[]
                 { "cat", "mouse", "lion", "zebra" }
             .OrderBy(x => x);
-        this.animals3.ItemsSource = this.animals4.ItemsSource = new[]
+        this.animals3.ItemsSource = new[]
+                { "cat", "zebra" }
+            .OrderBy(x => x);
+        this.animals4.ItemsSource = this.animals5.ItemsSource = this.animals6.ItemsSource = new[]
                 { "cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra", "tiger", "donkey" }
             .OrderBy(x => x);
     }

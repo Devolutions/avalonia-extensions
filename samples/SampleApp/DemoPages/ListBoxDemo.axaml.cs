c75511168f246c6b77ee90e38665c0ed06986b29
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-08-26 16:41:25 -0400

    [MacOS] ListBox styling

--- samples/SampleApp/DemoPages/ListBoxDemo.axaml.cs
+++ samples/SampleApp/DemoPages/ListBoxDemo.axaml.cs
@@ -12,10 +12,7 @@ public partial class ListBoxDemo : UserControl
         this.animals1.ItemsSource = this.animals2.ItemsSource = new[]
                 { "cat", "mouse", "lion", "zebra" }
             .OrderBy(x => x);
-        this.animals3.ItemsSource = new[]
-                { "cat", "zebra" }
-            .OrderBy(x => x);
-        this.animals4.ItemsSource = this.animals5.ItemsSource = this.animals6.ItemsSource = new[]
+        this.animals4.ItemsSource = this.animals5.ItemsSource = this.animals6.ItemsSource = this.animals7.ItemsSource = new[]
                 { "cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra", "tiger", "donkey" }
             .OrderBy(x => x);
     }
--- src/Devolutions.AvaloniaTheme.MacOS/Converters/MacOSConverters.cs
+++ src/Devolutions.AvaloniaTheme.MacOS/Converters/MacOSConverters.cs
@@ -1,6 +1,7 @@
 namespace Devolutions.AvaloniaTheme.MacOS.Converters;
 
-public static class MacOSConverters
+public static partial class MacOSConverters
 {
   public static readonly CharToMacOsPasswordCharConverter CharToMacOsPasswordCharConverter = new();
+  public static readonly IsVerticalMultiColumnListBoxConverter IsVerticalMultiColumnListBoxConverter = new();
 }

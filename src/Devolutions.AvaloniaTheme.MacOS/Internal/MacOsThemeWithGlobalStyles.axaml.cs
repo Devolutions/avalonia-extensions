e6e3f2a4c66c5be197cdae1acbd1e5e2e0b440b1
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-11-05 12:20:43 -0500

    Add MacOS version detection & dynamic loading of LiquidGlass Theme (incl. unused test override fct)

--- src/Devolutions.AvaloniaTheme.MacOS/Internal/MacOsThemeWithGlobalStyles.axaml.cs
+++ src/Devolutions.AvaloniaTheme.MacOS/Internal/MacOsThemeWithGlobalStyles.axaml.cs
@@ -3,15 +3,30 @@ namespace Devolutions.AvaloniaTheme.MacOS.Internal;
 using Avalonia.Markup.Xaml;
 using Avalonia.Markup.Xaml.Styling;
 using Avalonia.Styling;
+using Converters;
 
 internal class MacOsThemeWithGlobalStyles : Styles
 {
-    public MacOsThemeWithGlobalStyles(IServiceProvider? sp = null)
+  public MacOsThemeWithGlobalStyles(IServiceProvider? sp = null)
+  {
+    AvaloniaXamlLoader.Load(sp, this);
+
+    // 1) Always load the base theme
+    Uri baseUri = new("avares://Devolutions.AvaloniaTheme.MacOS/Accents/ThemeResources.axaml");
+    ResourceInclude baseInclude = new(baseUri) { Source = baseUri };
+    this.Resources.MergedDictionaries.Add(baseInclude);
+
+    // 2) Conditionally load LiquidGlass overrides
+    if (MacOSVersionDetector.IsLiquidGlassSupported())
     {
-        AvaloniaXamlLoader.Load(sp, this);
-#if DEBUG
-        Uri themePreviewerUri = new("avares://Devolutions.AvaloniaTheme.MacOS/Design/ThemePreviewer.axaml");
-        this.Resources.MergedDictionaries.Add(new ResourceInclude(themePreviewerUri) { Source = themePreviewerUri });
-#endif
+      Uri liquidGlassUri = new("avares://Devolutions.AvaloniaTheme.MacOS/Accents/ThemeResources_LiquidGlass.axaml");
+      ResourceInclude liquidGlassInclude = new(liquidGlassUri) { Source = liquidGlassUri };
+      this.Resources.MergedDictionaries.Add(liquidGlassInclude);
     }
+
+#if DEBUG
+    Uri themePreviewerUri = new("avares://Devolutions.AvaloniaTheme.MacOS/Design/ThemePreviewer.axaml");
+    this.Resources.MergedDictionaries.Add(new ResourceInclude(themePreviewerUri) { Source = themePreviewerUri });
+#endif
+  }
 }

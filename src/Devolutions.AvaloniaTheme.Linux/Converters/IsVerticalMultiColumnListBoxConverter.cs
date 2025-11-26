7610af5936505f878fb076242a0ee8e1b367a467
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-11-25 22:20:39 -0500

    [All Themes] Listbox fixes (#300)

---
+++ src/Devolutions.AvaloniaTheme.Linux/Converters/IsVerticalMultiColumnListBoxConverter.cs
@@ -0,0 +1,24 @@
+namespace Devolutions.AvaloniaTheme.Linux.Converters;
+
+using System.Globalization;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Layout;
+using Avalonia.Markup.Xaml.Templates;
+
+public class IsVerticalMultiColumnListBoxConverter : IValueConverter
+{
+  public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    if (value is ListBox { ItemsPanel : ItemsPanelTemplate template })
+    {
+      Panel? probe = template.Build(); // off-tree probe
+      if (probe is WrapPanel wp) return wp.Orientation == Orientation.Vertical;
+    }
+
+    return false;
+  }
+
+  public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+    throw new NotImplementedException();
+}

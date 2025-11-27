26d5641eafc022ef228e907201647bd66fceac73
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-11-27 18:42:01 -0500

    [DevExpress] Make Separator in menus default to custom margins

---
+++ src/Devolutions.AvaloniaControls/Converters/AnyNotNullToChoiceConverter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace Devolutions.AvaloniaControls.Converters;
+
+public class AnyNotNullToChoiceConverter : IMultiValueConverter
+{
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values == null || values.Count < 2)
+        {
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        var trueChoice = values[values.Count - 2];
+        var falseChoice = values[values.Count - 1];
+
+        for (int i = 0; i < values.Count - 2; i++)
+        {
+            var val = values[i];
+            if (val is not null && val != AvaloniaProperty.UnsetValue)
+            {
+                return trueChoice;
+            }
+        }
+
+        return falseChoice;
+    }
+}

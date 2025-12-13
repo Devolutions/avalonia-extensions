7321e6ea074cb2502b7f352b9f4467a4e2898aaa
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-12-12 23:44:22 -0500

    [DevExpress] Add very basic TreeDataGrid  styles & more complex Demo (#322)

--- samples/SampleApp/DemoPages/TreeDataGridDemo.axaml.cs
+++ samples/SampleApp/DemoPages/TreeDataGridDemo.axaml.cs
@@ -1,4 +1,8 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Controls.Models.TreeDataGrid;
+using Avalonia.Controls.Templates;
+using SampleApp.ViewModels;
 
 namespace SampleApp.DemoPages;
 
@@ -7,5 +11,37 @@ public partial class TreeDataGridDemo : UserControl
   public TreeDataGridDemo()
   {
     InitializeComponent();
+  }
+
+  protected override void OnDataContextChanged(EventArgs e)
+  {
+      base.OnDataContextChanged(e);
+      if (DataContext is TreeDataGridViewModel vm)
+      {
+          var template = this.FindResource("NameColumnTemplate") as IDataTemplate;
+          if (template == null) return;
+
+          // Add to CellSelectionSource
+          if (vm.CellSelectionSource != null && vm.CellSelectionSource.Columns.Count > 0 && vm.CellSelectionSource.Columns[0].Header as string != "Name")
+          {
+              var col = CreateNameExpanderColumn(template);
+              vm.CellSelectionSource.Columns.Insert(0, col);
+          }
+
+          // Add to RowSelectionSource
+          if (vm.RowSelectionSource != null && vm.RowSelectionSource.Columns.Count > 0 && vm.RowSelectionSource.Columns[0].Header as string != "Name")
+          {
+              var col = CreateNameExpanderColumn(template);
+              vm.RowSelectionSource.Columns.Insert(0, col);
+          }
+      }
+  }
+
+  private HierarchicalExpanderColumn<NetworkNode> CreateNameExpanderColumn(IDataTemplate template)
+  {
+      return new HierarchicalExpanderColumn<NetworkNode>(
+          new TemplateColumn<NetworkNode>("Name", template),
+          node => node.Children
+      );
   }
 }

8ee144218a9cea87b12203724588389228807aa6
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-12-19 18:18:49 -0500

    [MacOs] Fix tiny text box artifacts (#333)

--- src/Devolutions.AvaloniaTheme.MacOS/Converters/MacOSConverters.cs
+++ src/Devolutions.AvaloniaTheme.MacOS/Converters/MacOSConverters.cs
@@ -4,4 +4,5 @@ public static partial class MacOSConverters
 {
   public static readonly CharToMacOsPasswordCharConverter CharToMacOsPasswordCharConverter = new();
   public static readonly IsVerticalMultiColumnListBoxConverter IsVerticalMultiColumnListBoxConverter = new();
+  public static readonly AndToOpacityConverter AndToOpacityConverter = new();
 }

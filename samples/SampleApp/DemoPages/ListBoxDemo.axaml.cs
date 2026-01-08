588fa79dee5191501604b85038e1fae7991449b1
Xavier Fortin <xfortin@devolutions.net>
2026-01-08 14:45:16 -0500

    Demo of the issue with content of ListBoxItem not being able to stretch.

--- samples/SampleApp/DemoPages/ListBoxDemo.axaml.cs
+++ samples/SampleApp/DemoPages/ListBoxDemo.axaml.cs
@@ -15,5 +15,19 @@ public partial class ListBoxDemo : UserControl
         this.animals4.ItemsSource = this.animals5.ItemsSource = this.animals6.ItemsSource = this.animals7.ItemsSource = new[]
                 { "cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra", "tiger", "donkey" }
             .OrderBy(x => x);
+        this.animals8.ItemsSource = new[]
+        {
+            new Animal { Name = "Lion", Region = "Africa" },
+            new Animal { Name = "Panda", Region = "Asia" },
+            new Animal { Name = "Kangaroo", Region = "Australia" },
+            new Animal { Name = "Polar Bear", Region = "Arctic" }
+        };
+    }
+
+    public class Animal
+    {
+        public string Name { get; init; } = "";
+
+        public string Region { get; init; } = "";
     }
 }

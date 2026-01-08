2edf2bf800cf65079c44592b4a67240371123bc8
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2026-01-08 14:34:47 -0500

    [MacOS] Fix inconsistent response to custom height across controls (#334)

--- samples/SampleApp/DemoPages/ControlAlignment.axaml.cs
+++ samples/SampleApp/DemoPages/ControlAlignment.axaml.cs
@@ -8,19 +8,24 @@ public partial class ControlAlignment : UserControl
   public ControlAlignment()
   {
     this.InitializeComponent();
-    this.Animals.ItemsSource = this.Animals2.ItemsSource = this.Animals3.ItemsSource = this.Animals4.ItemsSource = new[]
+    this.Animals.ItemsSource = this.Animals2.ItemsSource = this.Animals3.ItemsSource = this.Animals4.ItemsSource = this.AnimalsCustomHeight.ItemsSource = new[]
         { "cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra" }
       .OrderBy(x => x);
 
     // Pre-populate horizontal TagInput controls with "A" tag for visibility
     if (this.HorizontalGridTagInput is not null)
     {
-      this.HorizontalGridTagInput.Tags.Add("A");
+      this.HorizontalGridTagInput.Tags.Add("Tag");
     }
 
     if (this.HorizontalStackPanelTagInput is not null)
     {
-      this.HorizontalStackPanelTagInput.Tags.Add("A");
+      this.HorizontalStackPanelTagInput.Tags.Add("Tag");
+    }
+
+    if (this.HorizontalStackPanelTagInputCustomHeight is not null)
+    {
+      this.HorizontalStackPanelTagInputCustomHeight.Tags.Add("Tag");
     }
   }
 }

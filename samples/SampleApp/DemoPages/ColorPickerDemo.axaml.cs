6af658ef4835fe130702a41f77262f85ca186fe4
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2026-01-13 22:09:05 -0500

    Add setup ColorPicker demo & copy control themes from Fluent

---
+++ samples/SampleApp/DemoPages/ColorPickerDemo.axaml.cs
@@ -0,0 +1,11 @@
+namespace SampleApp.DemoPages;
+
+using Avalonia.Controls;
+
+public partial class ColorPickerDemo : UserControl
+{
+  public ColorPickerDemo()
+  {
+    this.InitializeComponent();
+  }
+}

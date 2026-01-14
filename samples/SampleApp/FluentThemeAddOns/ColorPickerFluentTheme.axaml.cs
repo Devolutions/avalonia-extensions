d481946e99df97e367fa5b96e71847b7fdc4fef6
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2026-01-14 15:12:58 -0500

    Add Fluent theme for ColorPicker

---
+++ samples/SampleApp/FluentThemeAddOns/ColorPickerFluentTheme.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace SampleApp.FluentThemeAddOns;
+
+public class ColorPickerFluentTheme : Styles
+{
+    public ColorPickerFluentTheme() => AvaloniaXamlLoader.Load(this);
+}

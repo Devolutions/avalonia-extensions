60cae9e163be639a7738190c72f9bcd882528307
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2026-02-04 15:40:24 -0500

    Fix devexpress tabcontrol tab visibility issue (#364)

--- src/Devolutions.AvaloniaTheme.DevExpress/Converters/TabBorderVisibilityConverter.cs
+++ src/Devolutions.AvaloniaTheme.DevExpress/Converters/TabBorderVisibilityConverter.cs
@@ -1,4 +1,4 @@
-namespace Devolutions.AvaloniaControls.Converters;
+namespace Devolutions.AvaloniaTheme.DevExpress.Converters;
 
 using System.Globalization;
 using Avalonia.Controls;
@@ -8,37 +8,42 @@ using Avalonia.Data.Converters;
 ///   Takes a TabItem, its parent TabControl and the name of a Border element and returns a boolean to indicate if the
 ///   given Border element should be visible or not
 /// </summary>
-/// <param name="values">TabItem, TabControl, TabControl.SelectedIndex</param>
+/// <param name="values">TabItem, TabControl, TabControl.SelectedIndex, VisibilityVersion (optional)</param>
 /// <param name="parameter">the name of the Border element whose visibility is to be set</param>
 /// <returns>true if the Border element should be visible</returns>
 /// <remarks>
 ///   The 3rd element in the parameter array, TabControl.SelectedIndex, is necessary as a separate input, even though the
 ///   whole TabControl is also passed in. Unless this dynamically changing property is passed in, the Converter will only
 ///   execute once, rather than each time the selected tab changes.
+///   The 4th element, VisibilityVersion, is optional and used to trigger re-evaluation when any TabItem's visibility changes.
 /// </remarks>
 public class TabBorderVisibilityConverter : IMultiValueConverter
 {
     public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (values[0] is not TabItem tabItem ||
+        if (values.Count < 3 ||
+            values[0] is not TabItem tabItem ||
             values[1] is not TabControl tabControl ||
             values[2] is not int selectedTabIndex)
         {
             return true;
         }
 
+        // Note: values[3] (VisibilityVersion) is intentionally not used - it's just for triggering re-evaluation
+
         if (tabControl.TabStripPlacement == Dock.Left && parameter as string == "BottomLeftCorner")
         {
             return true;
         }
 
-        int currentTabIndex = GetTabIndex(tabControl, tabItem);
+        (int previousTabIndex, int currentTabIndex, int nextTabIndex) = GetTabIndexes(tabControl, tabItem);
 
         bool isFirstTab = currentTabIndex == 0;
         bool isLastTab = currentTabIndex == tabControl.Items.Count - 1;
         bool isSelected = selectedTabIndex == currentTabIndex;
-        bool previousTabIsSelected = selectedTabIndex == currentTabIndex - 1;
-        bool nextTabIsSelected = selectedTabIndex == currentTabIndex + 1;
+        bool previousTabIsSelected = selectedTabIndex == previousTabIndex;
+        bool nextTabIsSelected = selectedTabIndex == nextTabIndex;
+
         bool isLeftOfSelectedTab = currentTabIndex < selectedTabIndex;
         bool isRightOfSelectedTab = currentTabIndex > selectedTabIndex;
 
@@ -70,6 +75,29 @@ public class TabBorderVisibilityConverter : IMultiValueConverter
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
         throw new NotImplementedException();
 
-    private static int GetTabIndex(TabControl tabControl, TabItem tabItem) =>
-        tabControl.Items.Cast<object>().ToList().IndexOf(tabItem);
+    private static (int previousTabIndex, int tabIndex, int nextTabIndex) GetTabIndexes(TabControl tabControl, TabItem tabItem)
+    {
+        int previousTabIndex = -1, tabIndex = -1, nextTabIndex = -1;
+        for (int i = 0; i < tabControl.Items.Count; ++i)
+        {
+            if (tabControl.Items[i] is TabItem { IsVisible: true } tab)
+            {
+                if (tab == tabItem)
+                {
+                    tabIndex = i;
+                }
+                else if (tabIndex == -1)
+                {
+                    previousTabIndex = i;
+                }
+                else
+                {
+                    nextTabIndex = i;
+                    break;
+                }
+            }
+        }
+
+        return (previousTabIndex, tabIndex, nextTabIndex);
+    }
 }
--- src/Devolutions.AvaloniaTheme.DevExpress/Properties/AssemblyInfo.cs
+++ src/Devolutions.AvaloniaTheme.DevExpress/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Devolutions.AvaloniaTheme.DevExpress")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Devolutions.AvaloniaTheme.DevExpress.Behaviors")]

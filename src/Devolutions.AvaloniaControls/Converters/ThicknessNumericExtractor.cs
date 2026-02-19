ccff1f773d1fe5d96ba77af5a013ae28abfd5085
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2026-02-19 17:58:48 -0500

    WIP; !!! NOT WORKING !!!

---
+++ src/Devolutions.AvaloniaControls/Converters/ThicknessNumericExtractor.cs
@@ -0,0 +1,25 @@
+﻿namespace Devolutions.AvaloniaControls.Converters;
+
+using Avalonia;
+using Avalonia.Data.Converters;
+
+public static partial class DevoConverters
+{
+    public static FuncValueConverter<Thickness, ThicknessSide, double> ThicknessNumericExtractor { get; } = new(
+        static (thickness, side) => side switch {
+            ThicknessSide.Left => thickness.Left,
+            ThicknessSide.Top => thickness.Top,
+            ThicknessSide.Right => thickness.Right,
+            ThicknessSide.Bottom => thickness.Bottom,
+            _ => 0,
+        });
+}
+
+[Flags]
+public enum ThicknessSide
+{
+    Left   = 0b_0001,
+    Top    = 0b_0010,
+    Right  = 0b_0100,
+    Bottom = 0b_1000,
+}

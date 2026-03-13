785598756348e3a264981f7bb2d3621f42c84e2c
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2026-03-13 21:54:08 +0100

    [DevExpress & Linux] Increase min Thumb size in larger ScrollViews (#418)

--- src/Devolutions.AvaloniaControls/Behaviors/ScrollBarShortBehavior.cs
+++ src/Devolutions.AvaloniaControls/Behaviors/ScrollBarShortBehavior.cs
@@ -1,23 +1,26 @@
 namespace Devolutions.AvaloniaControls.Behaviors;
 
 using System;
+using System.Linq;
+using System.Reactive.Disposables;
 using System.Runtime.CompilerServices;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Layout;
+using Avalonia.VisualTree;
 
 public static class ScrollBarShortBehavior
 {
-    private static readonly ConditionalWeakTable<ScrollBar, IDisposable> Subscriptions = new();
+  private static readonly ConditionalWeakTable<ScrollBar, IDisposable> Subscriptions = [];
 
   public static readonly AttachedProperty<bool> EnabledProperty =
     AvaloniaProperty.RegisterAttached<ScrollBar, bool>("Enabled", typeof(ScrollBarShortBehavior));
 
-    public static readonly AttachedProperty<double> MinThumbSizeProperty =
-      AvaloniaProperty.RegisterAttached<ScrollBar, double>("MinThumbSize", typeof(ScrollBarShortBehavior), 43.0);
+  public static readonly AttachedProperty<double> ShortScrollBarMaxSizeProperty =
+    AvaloniaProperty.RegisterAttached<ScrollBar, double>("ShortScrollBarMaxSize", typeof(ScrollBarShortBehavior), 43.0);
 
-    static ScrollBarShortBehavior()
+  static ScrollBarShortBehavior()
   {
     EnabledProperty.Changed.Subscribe(args =>
     {
@@ -27,16 +30,21 @@ public static class ScrollBarShortBehavior
         {
           if (Subscriptions.TryGetValue(scrollBar, out _)) return;
 
-          var subscription = scrollBar.GetObservable(Visual.BoundsProperty).Subscribe(bounds =>
+          var boundsSubscription = scrollBar.GetObservable(Visual.BoundsProperty).Subscribe(bounds =>
           {
-            double relevantDimension = scrollBar.Orientation == Orientation.Vertical ? bounds.Height : bounds.Width;
-            if (relevantDimension is 0.0) return;
-
-              bool isShort = relevantDimension < scrollBar.GetValue(MinThumbSizeProperty);
-
-              ((IPseudoClasses)scrollBar.Classes).Set(":short", isShort);
+            ApplyShortClasses(scrollBar, bounds);
           });
 
+          // Subscribe to TemplateApplied to handle the initial-load case where BoundsProperty
+          // fires before the visual template is built. Once the template is applied, the Track
+          // exists in the visual tree and we can invalidate it to pick up the correct
+          // pseudo-class styles (MinHeight/MinWidth) right away, without waiting for a scroll event.
+          EventHandler<TemplateAppliedEventArgs> templateHandler = (_, _) => InvalidateTrack(scrollBar);
+          scrollBar.TemplateApplied += templateHandler;
+
+          var subscription = new CompositeDisposable(
+            boundsSubscription,
+            Disposable.Create(() => scrollBar.TemplateApplied -= templateHandler));
           Subscriptions.Add(scrollBar, subscription);
         }
         else
@@ -46,17 +54,49 @@ public static class ScrollBarShortBehavior
             subscription.Dispose();
             Subscriptions.Remove(scrollBar);
           }
-          ((IPseudoClasses)scrollBar.Classes).Set(":short", false);
+          var classes = (IPseudoClasses)scrollBar.Classes;
+          classes.Set(":short", false);
+          classes.Set(":veryshort", false);
         }
       }
     });
+  }
+
+  private static void ApplyShortClasses(ScrollBar scrollBar, Rect bounds)
+  {
+    double relevantDimension = scrollBar.Orientation == Orientation.Vertical ? bounds.Height : bounds.Width;
+    if (relevantDimension is 0.0) return;
+
+    double maxSize = scrollBar.GetValue(ShortScrollBarMaxSizeProperty);
+    bool isVeryShort = relevantDimension < maxSize;
+    bool isShort = relevantDimension < (maxSize * 2) && !isVeryShort;
+
+    var classes = (IPseudoClasses)scrollBar.Classes;
+    bool changed = classes.Contains(":veryshort") != isVeryShort || classes.Contains(":short") != isShort;
+    classes.Set(":veryshort", isVeryShort);
+    classes.Set(":short", isShort);
+
+    // ScrollBar template styles alter the Track's Thumb MinHeight/MinWidth on pseudo-class
+    // change. Explicitly invalidate the Track so it re-measures with the updated style values,
+    // since Avalonia does not always propagate pseudo-class-driven style changes into template
+    // children automatically.
+    if (changed)
+    {
+      InvalidateTrack(scrollBar);
+    }
+  }
+
+  private static void InvalidateTrack(ScrollBar scrollBar)
+  {
+    var track = scrollBar.GetVisualDescendants().OfType<Track>().FirstOrDefault();
+    track?.InvalidateMeasure();
   }
 
   public static void SetEnabled(ScrollBar element, bool value) => element.SetValue(EnabledProperty, value);
 
   public static bool GetEnabled(ScrollBar element) => element.GetValue(EnabledProperty);
 
-    public static void SetMinThumbSize(ScrollBar element, double value) => element.SetValue(MinThumbSizeProperty, value);
+  public static void SetShortScrollBarMaxSize(ScrollBar element, double value) => element.SetValue(ShortScrollBarMaxSizeProperty, value);
 
-    public static double GetMinThumbSize(ScrollBar element) => element.GetValue(MinThumbSizeProperty);
+  public static double GetShortScrollBarMaxSize(ScrollBar element) => element.GetValue(ShortScrollBarMaxSizeProperty);
 }

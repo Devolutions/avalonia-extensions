7c260608c34bc34674eb46f5045470982385d803
Xavier Fortin <xfortin@devolutions.net>
2026-03-17 08:28:26 -0400

    Back EnumPIcerTextOverrides with a Dictionary instead of a List.

--- src/Devolutions.AvaloniaControls/Controls/EnumPicker/EnumPickerTextOverrides.cs
+++ src/Devolutions.AvaloniaControls/Controls/EnumPicker/EnumPickerTextOverrides.cs
@@ -1,8 +1,8 @@
 namespace Devolutions.AvaloniaControls.Controls;
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 
 public class EnumPickerTextOverride<T> where T : struct, Enum
 {
@@ -11,34 +11,60 @@ public class EnumPickerTextOverride<T> where T : struct, Enum
     public required string Value { get; init; }
 }
 
-public class EnumPickerTextOverrides<T> : List<EnumPickerTextOverride<T>>, IReadOnlyDictionary<T, string>
+public class EnumPickerTextOverrides<T> : IReadOnlyDictionary<T, string>, ICollection<EnumPickerTextOverride<T>>
     where T : struct, Enum
 {
-    private List<EnumPickerTextOverride<T>> Items => this;
+    private readonly Dictionary<T, EnumPickerTextOverride<T>> textOverrides = new();
 
-    public string this[T key] => this.Items.First(x => x.Key.Equals(key)).Value;
+    IEnumerator<EnumPickerTextOverride<T>> IEnumerable<EnumPickerTextOverride<T>>.GetEnumerator() => this.textOverrides.Values.GetEnumerator();
 
-    IEnumerable<T> IReadOnlyDictionary<T, string>.Keys => this.Items.Select(x => x.Key);
+    public void Add(EnumPickerTextOverride<T> item)
+    {
+        this.textOverrides.TryAdd(item.Key, item);
+    }
 
-    IEnumerable<string> IReadOnlyDictionary<T, string>.Values => this.Items.Select(x => x.Value);
+    public void Clear()
+    {
+        this.textOverrides.Clear();
+    }
 
-    public bool ContainsKey(T key) => this.Items.Any(x => x.Key.Equals(key));
+    public bool Contains(EnumPickerTextOverride<T> item) => this.textOverrides.ContainsKey(item.Key);
+
+    public void CopyTo(EnumPickerTextOverride<T>[] array, int arrayIndex)
+    {
+        // Not required for the limited scope of this companion tool, we only really need Add()
+    }
+
+    public bool Remove(EnumPickerTextOverride<T> item) => this.textOverrides.Remove(item.Key);
+
+    int ICollection<EnumPickerTextOverride<T>>.Count => this.textOverrides.Count;
+
+    public bool IsReadOnly => false;
+
+    public string this[T key] => this.textOverrides[key].Value;
+
+    public IEnumerable<T> Keys => this.textOverrides.Keys;
+
+    public IEnumerable<string> Values => this.textOverrides.Values.Select(textOverride => textOverride.Value);
+
+    IEnumerator<KeyValuePair<T, string>> IEnumerable<KeyValuePair<T, string>>.GetEnumerator() =>
+        this.textOverrides.Select(pair => new KeyValuePair<T, string>(pair.Key, pair.Value.Value)).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<KeyValuePair<T, string>>)this).GetEnumerator();
+
+    int IReadOnlyCollection<KeyValuePair<T, string>>.Count => this.textOverrides.Count;
+
+    public bool ContainsKey(T key) => this.textOverrides.ContainsKey(key);
 
     public bool TryGetValue(T key, [MaybeNullWhen(false)] out string value)
     {
-        foreach (var item in this)
+        if (this.textOverrides.TryGetValue(key, out EnumPickerTextOverride<T>? textOverride))
         {
-            if (item.Key.Equals(key))
-            {
-                value = item.Value;
-                return true;
-            }
+            value = textOverride.Value;
+            return true;
         }
 
         value = null;
         return false;
     }
-
-    IEnumerator<KeyValuePair<T, string>> IEnumerable<KeyValuePair<T, string>>.GetEnumerator()
-        => ((List<EnumPickerTextOverride<T>>)this).Select(x => new KeyValuePair<T, string>(x.Key, x.Value)).GetEnumerator();
 }

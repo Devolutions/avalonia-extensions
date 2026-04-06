dc0c08b5893860039e9b607bc30312167229c369
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2026-04-06 21:50:31 +0200

    Add dedicated SplitButton demo page

---
+++ samples/SampleApp/DemoPages/SplitButtonDemo.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SampleApp.DemoPages;
+
+public partial class SplitButtonDemo : UserControl
+{
+    public SplitButtonDemo()
+    {
+        InitializeComponent();
+    }
+}

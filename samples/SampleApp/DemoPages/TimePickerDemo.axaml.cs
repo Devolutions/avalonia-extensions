64c9de3af30f68054555eb39c10bf0d57856030e
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2026-05-06 17:19:07 -0400

    Add TimePicker Demo and copy control template from Fluent

---
+++ samples/SampleApp/DemoPages/TimePickerDemo.axaml.cs
@@ -0,0 +1,11 @@
+namespace SampleApp.DemoPages;
+
+using Avalonia.Controls;
+
+public partial class TimePickerDemo : UserControl
+{
+  public TimePickerDemo()
+  {
+    this.InitializeComponent();
+  }
+}
---
+++ samples/SampleApp/ViewModels/TimePickerViewModel.cs
@@ -0,0 +1,19 @@
+namespace SampleApp.ViewModels;
+
+using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+public partial class TimePickerViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private TimeSpan? selectedTime;
+
+    public string SelectedTimeDisplay => this.SelectedTime.HasValue
+        ? $"Selected time: {DateTime.Today.Add(this.SelectedTime.Value):h:mm tt}"
+        : "Selected time: --:-- --";
+
+    partial void OnSelectedTimeChanged(TimeSpan? value)
+    {
+        this.OnPropertyChanged(nameof(this.SelectedTimeDisplay));
+    }
+}

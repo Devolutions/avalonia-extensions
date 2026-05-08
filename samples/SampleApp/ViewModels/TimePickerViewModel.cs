2c1ac27beb0d56085a2a806f07b57a6b2bd56e8e
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2026-05-08 15:47:33 -0400

    Remove redundant code

--- samples/SampleApp/ViewModels/TimePickerViewModel.cs
+++ samples/SampleApp/ViewModels/TimePickerViewModel.cs
@@ -8,12 +8,6 @@ public partial class TimePickerViewModel : ObservableObject
   [ObservableProperty]
   private TimeSpan? selectedTime;
 
-  [ObservableProperty]
-  private TimeSpan? segmentedSelectedTime = new(10, 15, 0);
-
-  [ObservableProperty]
-  private TimeSpan? segmentedSelectedTime24 = new(18, 45, 30);
-
   public string SelectedTimeDisplay => this.SelectedTime.HasValue
     ? $"Selected time: {DateTime.Today.Add(this.SelectedTime.Value):h:mm tt}"
     : "Selected time: --:-- --";
@@ -22,9 +16,4 @@ public partial class TimePickerViewModel : ObservableObject
   {
     this.OnPropertyChanged(nameof(this.SelectedTimeDisplay));
   }
-
-  private static string Format12Hour(TimeSpan? value, string label) =>
-    value.HasValue
-      ? $"{label}: {DateTime.Today.Add(value.Value):h:mm tt}"
-      : $"{label}: --:-- --";
 }

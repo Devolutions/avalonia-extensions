e8bf245e5c23590eed771de376077421c68dba99
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2026-06-23 17:11:22 -0400

    [SampleApp] Add demo for ToggleButton

---
+++ samples/SampleApp/DemoPages/ToggleButtonDemo.axaml.cs
@@ -0,0 +1,11 @@
+namespace SampleApp.DemoPages;
+
+using Avalonia.Controls;
+
+public partial class ToggleButtonDemo : UserControl
+{
+  public ToggleButtonDemo()
+  {
+    this.InitializeComponent();
+  }
+}

8b2e985551af133f743873b5625b9ae2a2df9d01
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2026-06-26 17:04:52 -0400

    [All themes] Add FlyoutPresenter styling (#567)

---
+++ samples/SampleApp/DemoPages/FlyoutPresenterDemo.axaml.cs
@@ -0,0 +1,11 @@
+namespace SampleApp.DemoPages;
+
+using Avalonia.Controls;
+
+public partial class FlyoutPresenterDemo : UserControl
+{
+  public FlyoutPresenterDemo()
+  {
+    this.InitializeComponent();
+  }
+}
